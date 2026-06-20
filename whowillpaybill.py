150cf5dd6e6099bb0c6c59eeace82ed2b294d1d6
yeturu anish <yeturuanish179@gmail.com>
2026-06-20 14:25:10 +0530

    Integrate Streamlit for bill payment display

--- whowillpaybill.py
+++ whowillpaybill.py
@@ -1,4 +1,5 @@
+import streamlit as st
 import random
 friends=["Anish","karthik","sai"]
 pay_bill=random.choice(friends)
-print(pay_bill+" will pay the bill today")
+st.write(pay_bill+" will pay the bill today")
